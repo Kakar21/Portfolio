4093020ccbd4554e471bdf22a666a0c1f4417e6f
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2024-03-08 20:16:44 +0100

    added my skills basic

--- src/app/main/my-skills/my-skills.component.ts
+++ src/app/main/my-skills/my-skills.component.ts
@@ -1,12 +1,54 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
-
+  skills = [
+    {
+      path: 'angular',
+      name: 'Angular'
+    },
+    {
+      path: 'typescript',
+      name: 'TypeScript'
+    },
+    {
+      path: 'javascript',
+      name: 'JavaScript'
+    },
+    {
+      path: 'html',
+      name: 'HTML'
+    },
+    {
+      path: 'css',
+      name: 'CSS'
+    },
+    {
+      path: 'firebase',
+      name: 'Firebase'
+    },
+    {
+      path: 'git',
+      name: 'Git'
+    },
+    {
+      path: 'scrum',
+      name: 'Scrum'
+    },
+    {
+      path: 'api',
+      name: 'Rest-Api'
+    },
+    {
+      path: 'material-design',
+      name: 'Material Design'
+    },
+  ];
 }
