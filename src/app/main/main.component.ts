b395f97fa8803203cc2e867d91a56fa2b8ab0a07
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2024-03-11 19:33:30 +0100

    added arrow details scroll animation

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
 import { ArrowComponent } from './arrow/arrow.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-main',
@@ -15,11 +16,30 @@ import { ArrowComponent } from './arrow/arrow.component';
     MySkillsComponent,
     PortfolioComponent,
     ContactComponent,
-    ArrowComponent
+    ArrowComponent,
+    NgStyle
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
 
+  scrolled = [{ scrolled: false }, { scrolled: false }, { scrolled: false}];
+
+  @HostListener("document:scroll")
+  scrollFn() {
+    let arrows = document.querySelectorAll('app-arrow');
+    let i = -1;
+    arrows.forEach( (arrow) => {
+      i++
+      let rect = arrow.getBoundingClientRect();
+      if (rect) {
+        if (rect.top < (window.innerHeight / 2))  {
+          this.scrolled[i].scrolled = true;
+        }
+      }
+    })
+  }
 }
+
+
