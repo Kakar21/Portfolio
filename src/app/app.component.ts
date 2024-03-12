89705ec1defb4d5baea59689cc90fa8ea0e52ae6
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2024-03-12 17:09:54 +0100

    added burger menu animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { MenuComponent } from './shared/components/menu/menu.component';
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, MenuComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MenuComponent, NgStyle, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
+  menuOpen = false;
+
+  public toggelMenu($event: any): void {
+    this.menuOpen = $event;
+  }
 }
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgStyle],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @Output() newOpenMenu = new EventEmitter<boolean>();
 
+  open = false;
+  public openMenu(value: boolean) {
+    this.open = value;
+
+    if (value) {
+      document.body.classList.add('overflow-h');
+    } else {
+      document.body.classList.remove('overflow-h');
+    }
+
+    this.newOpenMenu.emit(value);
+  }
 }
