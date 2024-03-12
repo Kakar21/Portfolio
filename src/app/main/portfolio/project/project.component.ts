8368c01d9a09f397be66793795224671287845cb
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2024-03-12 03:15:47 +0100

    added project hover animations

--- src/app/main/portfolio/project/project.component.ts
+++ src/app/main/portfolio/project/project.component.ts
@@ -1,10 +1,10 @@
-import { NgStyle } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [NgStyle],
+  imports: [NgStyle, NgClass],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
 })
