4640bf22921d948bd3137b7383022a5f6184074d
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2024-03-13 20:32:13 +0100

    added privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: MainComponent},
-    { path: 'legal-notice', component: LegalNoticeComponent}
+    { path: 'legal-notice', component: LegalNoticeComponent},
+    { path: 'privacy-policy', component: PrivacyPolicyComponent}
 ];
