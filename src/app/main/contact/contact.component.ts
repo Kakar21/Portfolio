4fbadf2bc7d4aaaa2a1b89cca2237fc679f22f57
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2024-03-13 15:37:22 +0100

    added form validation & error message

--- src/app/main/contact/contact.component.ts
+++ src/app/main/contact/contact.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+    privacyPolicy: false
+  }
+
+  onSubmit(ngForm: NgForm) {
+    if(ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData)
+    }
+  }
 }
