db3a5c80130bf9e4a288a6f181a747d00818b05f
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2024-03-15 03:48:58 +0100

    added contact form logic

--- src/app/main/contact/contact.component.ts
+++ src/app/main/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -10,16 +11,44 @@ import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 })
 export class ContactComponent {
 
+  http = inject(HttpClient);
+
   contactData = {
     name: "",
     email: "",
     message: "",
     privacyPolicy: false
-  }
+  };
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://kakar.dev/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
 
   onSubmit(ngForm: NgForm) {
-    if(ngForm.valid && ngForm.submitted) {
-      console.log(this.contactData)
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            // TODO: add send email success message
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      // For success testing
+      ngForm.resetForm();
     }
   }
 
@@ -29,3 +58,4 @@ export class ContactComponent {
     }
   }
 }
+
