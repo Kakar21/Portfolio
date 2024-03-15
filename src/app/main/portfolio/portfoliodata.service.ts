7d1569251ddcf236b51be99b9c13145f6d265897
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2024-03-15 22:31:21 +0100

    deployed projects and linked them

--- src/app/main/portfolio/portfoliodata.service.ts
+++ src/app/main/portfolio/portfoliodata.service.ts
@@ -10,32 +10,40 @@ export class PortfoliodataService {
   projectlist = [
     {
       name: 'Join',
-      path: 'sharkie',
+      path: 'join',
       number: '01',
       tools: 'JavaScript | HTML | CSS',
       description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-      link: '',
+      link: 'https://join.kakar.dev/',
       github: 'https://github.com/Kakar21/Join'
     },
     {
       name: 'Sharkie',
-      path: 'sharkie',
+      path: 'sharkie-01',
       number: '02',
       tools: 'JavaScript | HTML | CSS',
       description: 'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.',
-      link: '',
+      link: 'https://sharkie.kakar.dev/',
       github: 'https://github.com/Kakar21/Sharkie'
+    },
+    {
+      name: 'Portfolio',
+      path: 'sharkie',
+      number: '03',
+      tools: 'Angular | TypeScript | HTML | SCSS',
+      description: "This website is my personal showcase as a Front-End Developer. It presents my skills and projects elegantly to impress potential employers.",
+      link: '',
+      github: 'https://github.com/Kakar21/Portfolio'
     },
     {
       name: 'Simple CRM',
       path: 'sharkie',
-      number: '03',
+      number: '04',
       tools: 'Angular | TypeScript | HTML | SCSS | Firebase',
       description: 'A very Simple Customer Relationship Management system working with CRUD functionality. [COMING SOON]',
       link: '',
       github: ''
     },
     // TODO: change the paths
-    // TODO: Add live links
   ];
 }
