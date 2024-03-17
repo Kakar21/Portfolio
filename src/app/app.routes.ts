bc31b3cbea6f822c516152218c2a48db6e0485c8
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2024-03-17 03:49:01 +0100

    added legal notice & privacy policy german translation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { DatenschutzerklaerungComponent } from './datenschutzerklaerung/datenschutzerklaerung.component';
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
     { path: 'legal-notice', component: LegalNoticeComponent },
-    { path: 'privacy-policy', component: PrivacyPolicyComponent }
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
+    { path: 'impressum', component: ImpressumComponent },
+    { path: 'datenschutzerklaerung', component: DatenschutzerklaerungComponent },
 ];
 
---
+++ src/app/datenschutzerklaerung/datenschutzerklaerung.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datenschutzerklaerung',
+  standalone: true,
+  imports: [],
+  templateUrl: './datenschutzerklaerung.component.html',
+  styleUrl: './datenschutzerklaerung.component.scss'
+})
+export class DatenschutzerklaerungComponent {
+
+}
