890d18c369dd41631813eb59dc84f535f5f8a081
Ghulammustafa Kakar <mustafakakar01@yahoo.com>
2024-03-19 21:09:40 +0100

    fixed some responsivness bugs

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -69,3 +69,13 @@ export class MainComponent {
 }
 
 
+function _calculateScrollbarWidth() {
+  document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
+}
+// recalculate on resize
+window.addEventListener('resize', _calculateScrollbarWidth, false);
+// recalculate on dom load
+document.addEventListener('DOMContentLoaded', _calculateScrollbarWidth, false); 
+// recalculate on load (assets loaded as well)
+window.addEventListener('load', _calculateScrollbarWidth);
+
